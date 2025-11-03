5ae9398706d8acae5424601032010229e6d5c8d7
Yasin <awesome_yasin@protonmail.com>
2025-11-03 21:00:04 +0530

    Minor change

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,18 +4,18 @@ import { authenticate, authorizeRole } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/registration', signup);
-router.post('/verify-otp', verifyOtp);
-router.post('/login', login);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password', resetPassword);
+router.post('/registration', signup); // Done 
+router.post('/verify-otp', verifyOtp); // Done
+router.post('/login', login); // Done
+router.post('/forgot-password', forgotPassword); // Done
+router.post('/reset-password', resetPassword); // Done
 router.post(
   '/invite',
   authenticate,
   authorizeRole(['super_admin']),
   inviteAdmin
-);
-router.post('/complete-admin-signup', completeAdminSignup);
+); // Done
+router.post('/complete-admin-signup', completeAdminSignup); // Done
 router.get('/admin-list', authenticate, authorizeRole(['super_admin']), listAdmins);
-router.delete('remove-admin/:id', authenticate, authorizeRole(['super_admin']), removeAdmin);
+router.delete('/remove-admin/:id', authenticate, authorizeRole(['super_admin']), removeAdmin);
 export default router;
